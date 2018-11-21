8749cdddecb6cb36a4ed0f3bbc1b6a782604ed1c
Guoliang3 <guoliang3@datangmobile.cn>
2018-11-21 22:17:35 +0800

    init project

---
+++ DataAddin/EarthQuakeDep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace DataAddin
+{
+    public partial class EarthQuakeDep
+    {
+        private void EarthQuakeDep_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
